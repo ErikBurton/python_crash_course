72c695942bf254a1fba293c17fa341d61d3108af
Erik Burton <erikburton1@gmail.com>
2024-12-02 20:05:43 -0700

    User chooses when to quit.

---
+++ ch_7_user_input_while_loops/parrot_2.py
@@ -0,0 +1,7 @@
+prompt = input("\nTell me something, and I'll repeat it back to you: ")
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
