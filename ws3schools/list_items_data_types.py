09de8635626291d8a0829b892ea348704f03b2b6
Erik Burton <erikburton1@gmail.com>
2024-12-08 17:55:14 -0700

    data types

---
+++ ws3schools/list_items_data_types.py
@@ -0,0 +1,15 @@
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, True]
+
+print(list1, list2, list3)
+
+list1 = ["abc", 34, True, 40, "car"]
+print(list1)
+
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = list(("apple", "banana", 'cherry')) # Note the double round-brackets
+print(thislist)
