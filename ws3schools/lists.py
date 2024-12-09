5262485796c41a7e694447ba698d3d5382961aa8
Erik Burton <erikburton1@gmail.com>
2024-12-08 17:48:27 -0700

    Working with lists.

---
+++ ws3schools/lists.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Lists will allow duplicates
+thislist = ["apple", "banana", "cherry", "apple"]
+print(thislist)
+
+# This will count the number or length of items in a list.
+thislist = ["apple", "banana", "cherry", "apple"]
+print(f"The number of itmes in this list is: {len(thislist)}")
